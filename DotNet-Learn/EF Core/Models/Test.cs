1f64a319356572457416eafc2df232301e71869c
Mohan k v <kvm8105@gmail.com>
2026-02-05 15:15:29 +0530

    EF core learn code update

---
+++ DotNet-Learn/EF Core/Models/Test.cs
@@ -0,0 +1,13 @@
+﻿namespace EF_Core.Models
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public string Data1 { get; set; }
+        public float Data2 { get; set; }
+        public double Data3 { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public bool IsDeleted { get; set; }
+        public virtual List<TestDetails> TestDetails { get; set; } = new List<TestDetails>();
+    }
+}
---
+++ DotNet-Learn/EF Core/Models/TestDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace EF_Core.Models
+{
+    public class TestDetails
+    {
+        public int Id { get; set; }
+        public int TestId { get; set; }
+        public string DetailData1 { get; set; }
+        public string DetailData2 { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public bool IsDeleted { get; set; }
+        public virtual Test Test { get; set; }
+    }
+}
