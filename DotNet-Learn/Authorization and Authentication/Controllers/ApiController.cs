f9d64ba584f594737c7090a42885e51205e2997a
Mohan k v <kvm8105@gmail.com>
2026-02-05 15:01:36 +0530

    updated code

--- DotNet-Learn/Authorization and Authentication/Controllers/ApiController.cs
+++ DotNet-Learn/Authorization and Authentication/Controllers/ApiController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 
 namespace Authorization_and_Authentication.Controllers
 {
@@ -13,12 +16,19 @@ namespace Authorization_and_Authentication.Controllers
         [AllowAnonymous]
         public IActionResult Login()
         {
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.Name, "TestUser"),
-                new Claim(ClaimTypes.Role, "Admin"),
-                new Claim("CustomClaimType", "CustomClaimValue")
-            };
+            var claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Role, "role"),
+                    new Claim("UserId", "UserId"),
+                    new Claim(ClaimTypes.Name, "Name"),
+                    new Claim(JwtRegisteredClaimNames.Sub, "Email"),
+                    new Claim(JwtRegisteredClaimNames.Jti,$"{Guid.NewGuid()}"),
+                };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("0123456789ABCDEF0123456789ABCDEF"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken("https://localhost:7047", "https://localhost:7047", claims, expires: DateTime.UtcNow.AddMinutes(Convert.ToInt64(20)), signingCredentials: creds);
+            var AccessToken = new JwtSecurityTokenHandler().WriteToken(token);
             return Ok("Authenticated");
         }
         public IActionResult Home()
