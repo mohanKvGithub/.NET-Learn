922f5a6b574808eeee7b2d2dffbf3bb84bbde9dc
Mohan k v <kvm8105@gmail.com>
2026-02-06 18:22:54 +0530

    updated code

---
+++ DotNet-Learn/EF Core/Models/AppContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF_Core.Models;
+
+public class AppContext:DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MyDatabase;Trusted_Connection=True;");
+    }
+}
