96a3a8eff010cb64eed585a5e4eaff46b0ee1141
Mohan k v <kvm8105@gmail.com>
2026-03-17 16:08:40 +0530

    updated code

--- DotNet-Learn/Sql Learn/SQLQuery.sql
+++ DotNet-Learn/Sql Learn/SQLQuery.sql
@@ -1,1 +1,14 @@
-Select FirstName, LastName, Count(*) as c from Customers group by FirstName, LastName
+use [.NetLearn]
+--------------------------------- Group By ---------------------------------
+go
+select FirstName from Customers group by FirstName
+go
+select FirstName, LastName from Customers group by FirstName, LastName
+
+--------------------------------- Order By ---------------------------------
+go
+select * from Customers order by LastName
+go
+select * from Customers order by LastName desc
+go
+select * from Customers order by Email,LastName 
