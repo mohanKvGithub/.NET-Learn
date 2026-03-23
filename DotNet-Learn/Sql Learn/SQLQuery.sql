79f18f7b8ed907550a6f8091bdbe77bf4d171805
Mohan k v <kvm8105@gmail.com>
2026-03-23 14:12:48 +0530

    updated code

--- DotNet-Learn/Sql Learn/SQLQuery.sql
+++ DotNet-Learn/Sql Learn/SQLQuery.sql
@@ -1,9 +1,4 @@
 use [.NetLearn]
---------------------------------- Group By ---------------------------------
-go
-select FirstName from Customers group by FirstName
-go
-select FirstName, LastName from Customers group by FirstName, LastName
 
 --------------------------------- Order By ---------------------------------
 go
@@ -11,4 +6,34 @@ select * from Customers order by LastName
 go
 select * from Customers order by LastName desc
 go
-select * from Customers order by Email,LastName 
+select * from Customers order by Email,LastName
+go
+select * from Numbers order by num1 desc, num2 asc
+go
+select * from Numbers order by num1 desc, num2
+
+--------------------------------- Group By ---------------------------------
+go
+select FirstName from Customers group by FirstName
+go
+select FirstName, LastName from Customers group by FirstName, LastName
+go
+select oi.OrderId,o.CustomerId, count(*) as p from Orders o Left join OrderItems oi on o.OrderId=oi.OrderId
+group by oi.OrderId,o.CustomerId
+
+--------------------------------- Aggregate Functions ---------------------------------
+-- Aggregate function are performed on list of rows 
+
+--	1) MIN()
+go
+select min(UnitPrice) as minPrice from OrderItems group by Quantity
+go
+select min(Price) as minPrice from Products
+
+--	2) MAX()
+go
+select max(UnitPrice) as maxPrice from OrderItems group by Quantity
+go
+select max(Price) as maxPrice from Products
+
+--	3) COUNT()
