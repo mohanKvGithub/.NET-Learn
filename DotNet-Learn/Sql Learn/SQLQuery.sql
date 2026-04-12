086c72cf256da63c211ee767abd5f929757b4ca9
Mohan k v <kvm8105@gmail.com>
2026-04-12 23:38:15 +0530

    updated code

--- DotNet-Learn/Sql Learn/SQLQuery.sql
+++ DotNet-Learn/Sql Learn/SQLQuery.sql
@@ -66,21 +66,21 @@ group by oi.OrderId,o.CustomerId
 -- Aggregate Functions : are performed on list of rows (grouped data or table)
 -- ======================================================================================================================================================
 
---	1) MIN() works with numeric, string, and date data types. returns the smallest value of the selected column.
+	--	1) MIN() works with numeric, string, and date data types. returns the smallest value of the selected column.
 go
 select min(UnitPrice) as minPrice from OrderItems group by Quantity
 go
 select min(Price) as minPrice from Products
 
 -- ======================================================================================================================================================
---	2) MAX() works with numeric, string, and date data types. returns the largest value of the selected column.
+	--	2) MAX() works with numeric, string, and date data types. returns the largest value of the selected column.
 go
 select max(UnitPrice) as maxPrice from OrderItems group by Quantity
 go
 select max(Price) as maxPrice from Products
 
 -- ======================================================================================================================================================
---	3) COUNT()
+	--	3) COUNT()
 go 
 select count(*) from Products -- Counts Total no rows including null
 go
@@ -92,7 +92,7 @@ select IsDeleted as Deleted, count(*) as Count from Products group by IsDeleted;
 go
 
 -- ======================================================================================================================================================
---	4) SUM() used to calculate the total sum of values within a specified numeric column. Ignores null value
+	--	4) SUM() used to calculate the total sum of values within a specified numeric column. Ignores null value
 
 select sum(StockQuantity) from Products;
 go
@@ -100,9 +100,44 @@ select sum(StockQuantity) from Products group by IsDeleted
 go
 
 -- ======================================================================================================================================================
---	4) AVG() : SUM(column)/COUNT(column) = equal share for each row. Ignores null value
+	--	5) AVG() : SUM(column)/COUNT(column) = equal share for each row. Ignores null value
 
 select avg(Price) from Products 
 go
 select avg(Price) from Products group by Category
+go
 
+-- ======================================================================================================================================================
+-- HAVING
+-- ======================================================================================================================================================
+	--to filter using aggregate functions after grouping
+select PaymentMethod, count(*) as TotalPayments from Payments group by PaymentMethod having sum(Amount) >10000 and sum(Amount) <50000
+go
+select PaymentMethod from Payments group by PaymentMethod having count(*) > 10
+go
+select PaymentMethod from Payments group by PaymentMethod having min(Amount)>10000 
+go
+select PaymentMethod from Payments group by PaymentMethod having max(Amount)>10000 
+go
+-- ======================================================================================================================================================
+-- EXISTS 
+-- ======================================================================================================================================================
+	--used in a WHERE clause to check whether a subquery returns any rows.
+select * from Customers c where exists(select * from Orders o where o.CustomerId=c.CustomerId and o.OrderStatus='Completed')
+go
+-- ======================================================================================================================================================
+-- ANY 
+-- ======================================================================================================================================================
+	--TRUE if at least one value in the subquery result-set meet the condition.
+select * from Customers c where c.CustomerId = any(select CustomerId from Orders o left join Payments p on p.OrderId=o.OrderId where o.CustomerId=CustomerId and p.PaymentMethod='Cash');
+go
+select * from Customers c where c.CustomerId = any(select o.CustomerId from Orders o);
+go
+-- ======================================================================================================================================================
+-- ALL 
+-- ======================================================================================================================================================
+	--TRUE if every value in the subquery result-set meet the condition.
+go
+select * from Products p where 'Completed'=all(select OrderStatus from Orders o left join OrderItems oi on oi.OrderId=o.OrderId where oi.ProductId=p.ProductId )
+go
+select * from Products p where p.StockQuantity > all(select sum(Quantity) from OrderItems o where o.ProductId=p.ProductId)
